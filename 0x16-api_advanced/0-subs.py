ab8b84d195884614e27309bdb6f291589d2b677e
root <rehamhakeem.official@gmail.com>
2024-08-13 15:37:23 +0000

    all

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python3
-"""This script will return the number of subscribers associated with
-a subreddit
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Method get the number of users subscribed to a subreddit
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
 
-    subreddit (Str)- subreddit to check
-
-    Returns - number of users (INT) else 0 (INT) if not subreddit is found
-    """
-    try:
-        h = {'user-agent': 'Mozilla/5.0', 'allow_redirects': 'false'}
-        url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-        req = requests.get(url, headers=h)
-        return req.json().get('data').get('subscribers', 0)
-    except Exception as e:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
